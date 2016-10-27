4f511da9acdd4afe9758e689b50e14accbb782e9
Gonzo <ghanemgo@gmail.com>
2016-10-27 20:28:08 -0300

    VERSION PARA EL CIITI

--- public/js/prefabs/Asteroid.js
+++ public/js/prefabs/Asteroid.js
@@ -1,36 +1,21 @@
 var Chaos = Chaos || {};
 
-Chaos.Asteroid = function(game, x, y, key) {
+Chaos.Asteroid = function(game, x, y, key, target) {
   Phaser.Sprite.call(this, game, x, y, key);
 
   this.game = game;
+  this.target = target;
   //enable physics
-  this.game.physics.arcade.enable(this);
-  this.enableBody = true;
   this.anchor.setTo(0.5);
   this.checkWorldBounds = true;
-  // this.events.onOutOfBounds.add(this.kill, this);
   this.outOfBoundsKill = true;
 };
 
 Chaos.Asteroid.prototype = Object.create(Phaser.Sprite.prototype);
 Chaos.Asteroid.prototype.constructor = Chaos.Asteroid;
 
-//   if(this.position.x < 0.05 * this.game.world.width) {
-//     this.position.x = 0.05 * this.game.world.width + 2;
-//     this.body.velocity.x *= -1;
-//   }
-//   else if(this.position.x > 0.95 * this.game.world.width) {
-//     this.position.x = 0.95 * this.game.world.width - 2;
-//     this.body.velocity.x *= -1;
-//   }
-
-//   if(this.position.y > this.game.world.height) {
-//     this.position.y = 0.95 * this.game.world.height + 2;
-//     this.body.velocity.y *= -1;
-//   }
-//   if(this.position.y < this.game.world.bounds.top) {
-//     this.position.y = 0.05 * this.game.world.height - 2;
-//     this.body.velocity.y *= -1;
-//   }
-// };
+Chaos.Asteroid.prototype.update = function() {
+  this.game.physics.arcade.moveToXY(this, this.target.x + 20, this.target.y + 20, 100);
+  // var destination = this.game.world.bounds.bottomRight;
+  // this.game.physics.arcade.moveToXY(this, destination.x + 20, destination.y + 20, 100);
+};
